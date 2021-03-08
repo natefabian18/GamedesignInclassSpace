13b017b9c8d9bd3d8a0d0493529034d9c1b02b70
Nate Fabian <nathan.fabian@stvincent.edu>
2021-03-08 11:43:14 -0500

    Shifted Project to Photon testing project

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//added for photon
+using Photon.Pun;
+using Photon.Realtime;
+
+using UnityEngine.UI;
+
+public class Launcher : MonoBehaviourPunCallbacks//MonoBehaviour
+{
+	string gameVersion = "0.1"; //version
+	public byte maxPlayersPerRoom = 2; //players in game
+	private bool isConnecting;
+	private void Awake()
+	{
+		PhotonNetwork.AutomaticallySyncScene = true;
+	}
+	void Start()
+	{
+		Connect();
+	}
+	void Update()
+	{
+
+	}
+
+	//photon session handling
+
+	public void Disconnect() {
+		PhotonNetwork.Disconnect();
+	}
+	public void Connect() {
+		isConnecting = PhotonNetwork.ConnectUsingSettings();
+		if (PhotonNetwork.IsConnected)
+		{
+			PhotonNetwork.JoinRandomRoom();
+		}
+		else {
+			//create room
+			PhotonNetwork.ConnectUsingSettings();
+			PhotonNetwork.GameVersion = gameVersion;
+		}
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		base.OnConnectedToMaster();
+		Debug.Log("connected to master");
+		if (isConnecting) {
+			PhotonNetwork.JoinRandomRoom();
+			isConnecting = false;
+		}
+	}
+
+	public override void OnDisconnected(DisconnectCause cause)
+	{
+		base.OnDisconnected(cause);
+		Debug.Log($"disconnected for cause {cause.ToString()}");
+	}
+
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		base.OnJoinRandomFailed(returnCode, message);
+		Debug.Log("no room found, so create one");
+		PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+	}
+	public override void OnLeftRoom()
+	{
+		base.OnLeftRoom();
+		Debug.Log("player left room");
+	}
+
+	public override void OnJoinedRoom()
+	{
+		base.OnJoinedRoom();
+		Debug.Log("Player Joined Room");
+	}
+}
